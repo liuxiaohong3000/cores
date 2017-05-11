08b9fd0164496080fbdd18480bddd04529eb392f
liuxiaohong <liuxiaohong@kokozu.net>
2017-05-11 13:33:50 +0800

    第一版代码提交

---
+++ env/src/main/java/com/open/cores/App.java
@@ -0,0 +1,13 @@
+package com.open.cores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
